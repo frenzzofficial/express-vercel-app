59005b2aeda5444d30637857d022c294561fcc5f
vivek <vivekcse.in@gmail.com>
2025-10-04 18:48:25 +0530

    request Id is setup properly

--- src/libs/middlewares/requestId.ts
+++ src/libs/middlewares/requestId.ts
@@ -1,4 +1,3 @@
-// libs/middlewares/requestId.ts
 import { v4 as uuidv4 } from "uuid";
 import type { Request, Response, NextFunction } from "express";
 
@@ -7,10 +6,7 @@ const requestIdMiddleware = (req: Request, res: Response, next: NextFunction) =>
   const requestId =
     typeof incomingId === "string" && incomingId.trim() !== "" ? incomingId : uuidv4();
 
-  req.headers["x-request-id"] = requestId;
   res.setHeader("X-Request-ID", requestId);
-
-  // Optional: attach to req object for logging
   (req as any).requestId = requestId;
 
   next();
