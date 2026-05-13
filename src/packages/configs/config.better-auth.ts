62bc64bf0d0b169aab519349db0753de5976513a
vivekcsein <vivekcse.in@gmail.com>
2026-05-14 02:37:00 +0530

    no real token

--- src/packages/configs/config.better-auth.ts
+++ src/packages/configs/config.better-auth.ts
@@ -1,28 +1,28 @@
-import { betterAuth } from "better-auth";
+// import { betterAuth } from "better-auth";
 
-export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
+// export const auth = betterAuth({
+//   emailAndPassword: {
+//     enabled: true,
+//   },
 
-  session: {
-    expiresIn: 60 * 60 * 24 * 30, // 30 days
-    updateAge: 60 * 60 * 24, // refresh daily
-  },
+//   session: {
+//     expiresIn: 60 * 60 * 24 * 30, // 30 days
+//     updateAge: 60 * 60 * 24, // refresh daily
+//   },
 
-  advanced: {
-    useSecureCookies: process.env.NODE_ENV === "production",
-    crossSubDomainCookies: {
-      enabled: false,
-    },
-  },
+//   advanced: {
+//     useSecureCookies: process.env.NODE_ENV === "production",
+//     crossSubDomainCookies: {
+//       enabled: false,
+//     },
+//   },
 
-  socialProviders: {
-    // future
-    // google: {},
-    // discord: {},
-  },
-});
+//   socialProviders: {
+//     // future
+//     // google: {},
+//     // discord: {},
+//   },
+// });
 
 export const ACCESS_TOKEN_COOKIE = "access_token";
 
